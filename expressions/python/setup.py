872c83fabf89f010d9e71b8739fea4878b1676db
Rowan Seymour <rowanseymour@gmail.com>
2015-09-23 12:48:44 +0200

    Updated version to 1.0.4

--- expressions/python/setup.py
+++ expressions/python/setup.py
@@ -19,7 +19,7 @@ test_packages = _read_requirements("requirements/tests.txt")
 
 setup(
     name='rapidpro-expressions',
-    version='1.0.3',
+    version='1.0.4',
     description='Python implementation of the RapidPro expression and templating system',
     url='https://github.com/rapidpro/flows',
 
