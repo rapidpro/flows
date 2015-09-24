486870d30a38ff8a6b41ef6193e23e4ebc9e3812
Rowan Seymour <rowanseymour@gmail.com>
2015-09-24 11:04:43 +0200

    Added method to extract the current auto-complete context

--- expressions/js/excellent.js
+++ expressions/js/excellent.js
@@ -1,41 +1,125 @@
 /**
  * Javascript parser for Excellent-style expressions used in RapidPro
  */
-(function(excellent, $) {
+(function(excellent) {
 
     var STATE_BODY = 0;               // not in a expression
     var STATE_PREFIX = 1;             // '@' prefix that denotes the start of an expression
-    var STATE_IDENTIFIER = 2;         // the identifier part, e.g. 'SUM' in '@SUM(1, 2)' or 'contact.age' in '@contact.age'
+    var STATE_IDENTIFIER = 2;         // the identifier part, e.g. 'contact.age' in '@contact.age'
     var STATE_BALANCED = 3;           // the balanced parentheses delimited part, e.g. '(1 + 2)' in '@(1 + 2)'
     var STATE_STRING_LITERAL = 4;     // a string literal which could contain )
     var STATE_ESCAPED_PREFIX = 5;     // a '@' prefix preceded by another '@'
+    var STATE_IGNORE = 6;
 
+    /**
+     * Creates a new parser
+     * @param expressionPrefix the prefix for expressions, e.g. '@'
+     * @param allowedTopLevels the context names that are allowed without parentheses, e.g. ["contact", "flow", ...]
+     */
     excellent.Parser = function(expressionPrefix, allowedTopLevels) {
         this.expressionPrefix = expressionPrefix;
         this.allowedTopLevels = allowedTopLevels;
     };
 
-    excellent.Parser.prototype.findExpressionContext = function(text) {
-        expressions = this.findExpressions(text);
+    /**
+     * Given the text up to the caret position, returns the expression currently being edited, without its prefix
+     */
+    excellent.Parser.prototype.expressionContext = function(textToCaret) {
+        expressions = this.expressions(textToCaret);
 
-        if (expressions.length == 0) {
+        if (expressions.length == 0) { // no expressions found
             return null;
         }
 
         var lastExpression = expressions[expressions.length - 1];
 
-        if (lastExpression.end < text.length || lastExpression.complete) {
+        // has last expression already ended or is it closed (i.e. has balanced parentheses)
+        if (lastExpression.end < textToCaret.length || lastExpression.closed) {
             return null;
         }
 
         return lastExpression.text.substring(1);  // return without prefix
     };
 
-    excellent.Parser.prototype.findNameContext = function(expression) {
-        // TODO
+    /**
+     * Given the partial expression currently being edited, returns the current auto-completable identifier
+     * which may be a function name or a context reference.
+     */
+    excellent.Parser.prototype.autoCompleteContext = function(partialExpression) {
+        if (isInStringLiteral(partialExpression)) {
+            return null;
+        }
+
+        var identifier = "";
+        
+        for (var pos = partialExpression.length - 1; pos >= 0; pos--) {
+            var ch = partialExpression[pos];
+            if (isWordChar(ch) || ch === '.') {
+                identifier = ch + identifier;
+            }
+            else {
+                break;
+            }
+        }
+
+        if (identifier.match(/[A-Za-z][\w]*(\.[\w]+)*/)) {
+            return identifier;
+        }
+        else {
+            return null;
+        }
     };
 
-    excellent.Parser.prototype.findExpressions = function(text) {
+    /**
+     * TODO find the function context
+     */
+    excellent.Parser.prototype.functionContext = function(partialExpression) {
+        var inString = isInStringLiteral(partialExpression);
+
+        // initial state is string literal if number of quotes is odd
+        var state = inString ? STATE_IGNORE : STATE_STRING_LITERAL;
+        var identifier = "";
+        var parenthesesLevel = 0;
+        
+        for (var pos = partialExpression.length - 1; pos >= 0; pos--) {
+            var ch = partialExpression[pos];
+
+            if (state == STATE_IGNORE) {
+                if (parenthesesLevel == 0 && (isWordChar(ch) || ch === '.')) {
+                    state = STATE_IDENTIFIER;
+                    identifier = ch + identifier;
+                } 
+                else if (ch == "\"") {
+                    state = STATE_STRING_LITERAL;
+                }
+                else if (ch === '(') {
+                    parenthesesLevel--;
+                }
+                else if (ch === ')') {
+                    parenthesesLevel++;
+                }
+            }
+            else if (state == STATE_IDENTIFIER) {
+                if (isWordChar(ch) || ch === '.') {
+                    identifier = ch + identifier;
+                } 
+                else {
+                    return identifier;
+                }
+            }
+            else if (state == STATE_STRING_LITERAL) {
+                if (ch == "\"") {
+                    state = STATE_IGNORE;
+                }    
+            }
+        }
+        return null;
+    };
+
+    /**
+     * Finds all expressions in the given text, including a partially complete expression at the end of the input
+     */
+    excellent.Parser.prototype.expressions = function(text) {
         var expressions = [];
         var state = STATE_BODY;
         var currentExpression = null;
@@ -109,8 +193,8 @@
             if (currentExpression != null && (currentExpression.end != null || nextCh === 0)) {
                 var allowIncomplete = (nextCh === 0); // if we're at the end of the input, allow incomplete expressions
 
-                if (isValidExpression(currentExpression.text, this.allowedTopLevels, allowIncomplete)) {
-                	currentExpression.complete = (currentExpression.text[1] === '(') && (parenthesesLevel == 0);
+                if (isValidStart(currentExpression.text, this.allowedTopLevels, allowIncomplete)) {
+                	currentExpression.closed = (currentExpression.text[1] === '(') && (parenthesesLevel == 0);
                     currentExpression.end = pos + 1;
                     expressions.push(currentExpression);
                 }
@@ -124,10 +208,10 @@
     };
 
     /**
-     * Checks the parsed expression to determine if it's valid
+     * Checks the parsed (possibly partial) expression to determine if it's valid based on how it starts
      */
-    function isValidExpression(expression, allowedTopLevels, allowIncomplete) {
-        var body = expression.substring(1); // strip prefix
+    function isValidStart(partialExpression, allowedTopLevels, allowIncomplete) {
+        var body = partialExpression.substring(1); // strip prefix
 
         if (body[0] === '(') {
             return true;
@@ -149,6 +233,20 @@
     }
 
     /**
+     * Determines whether we are in a string literal
+     */
+    function isInStringLiteral(partialExpression) {
+        // count number quotation marks
+        var num_quotes = 0;
+        for (var pos = 0; pos < partialExpression.length; pos++) {
+            if (partialExpression[pos] === '"') {
+                num_quotes++;
+            }
+        }
+        return num_quotes % 2 != 0;  // odd means last string literal is open
+    }
+
+    /**
      * Determines whether the given string starts with the given text
      */
     function startsWith(str, start) {
@@ -162,4 +260,4 @@
         return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || ch == '_'; 
     }
 
-}(window.excellent = window.excellent || {}, jQuery));
+}(window.excellent = window.excellent || {}));
