990a75dbe2e13522df6d855a7ba1b6bca1c83d12
Rowan Seymour <rowanseymour@gmail.com>
2015-09-30 11:07:21 +0200

    Added skeleton python project and travis CI configuration

---
+++ python/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+
+def _is_requirement(line):
+    """Returns whether the line is a valid package requirement."""
+    line = line.strip()
+    return line and not (line.startswith("-r") or line.startswith("#"))
+
+
+def _read_requirements(filename):
+    """Returns a list of package requirements read from the file."""
+    requirements_file = open(filename).read()
+    return [line.strip() for line in requirements_file.splitlines()
+            if _is_requirement(line)]
+
+
+required_packages = _read_requirements("requirements/base.txt")
+test_packages = _read_requirements("requirements/tests.txt")
+
+setup(
+    name='rapidpro-flows',
+    version='0.1',
+    description='Python implementation of the RapidPro flow engine',
+    url='https://github.com/rapidpro/flows',
+
+    author='Nyaruka',
+    author_email='code@nyaruka.com',
+
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+    ],
+
+    keywords='rapidpro flows',
+    packages=find_packages(),
+    install_requires=required_packages,
+
+    test_suite='nose.collector',
+    tests_require=required_packages + test_packages,
+)
---
+++ python/temba_flows/tests.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from __future__ import absolute_import, unicode_literals
+
+
+import unittest
+
+
+class FlowsTests(unittest.TestCase):
+
+    def test_todo(self):
+        pass
