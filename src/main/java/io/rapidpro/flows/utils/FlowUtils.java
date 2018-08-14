c12329ee024a9a9dff2e3efb68863f4babf4f10e
Rowan Seymour <rowanseymour@gmail.com>
2018-08-14 12:05:53 -0500

    Update versions of expressions and phonenumbers

--- src/main/java/io/rapidpro/flows/utils/FlowUtils.java
+++ src/main/java/io/rapidpro/flows/utils/FlowUtils.java
@@ -7,9 +7,6 @@ import org.apache.commons.lang3.text.WordUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.lang.reflect.Field;
-import java.util.regex.Pattern;
-
 /**
  * Miscellaneous utility methods
  */
